aaa81f3850e9ceb42d7417376bb38f9d43d5add4
Mikhail Garkusha <mgarkusha@mail.ru>
2016-10-14 16:55:23 +0300

    Update

--- Урок4/hw04_normal.py
+++ Урок4/hw04_normal.py
@@ -1,8 +1,9 @@
 import re
+
 # Задание-1:
 # Вывести символы в нижнем регистре, которые окружают 1 или более символа в верхнем регистре.
 # Решить задачу двумя способами: с помощью re и без.
-line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalp' \
+line = 'ZtmMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalp' \
        'PLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzKTUfSYtBydXaVIpxWjNKgXANv' \
        'IoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkp' \
        'YOlqXEFIxPqqqgAWdJsOvqppOfyIVjXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSA' \
@@ -15,26 +16,75 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIP
        'GPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXi' \
        'UWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+# Почему это не хочет работать?
+# print(re.findall('(?<=[A-Z]{1,})[a-z]{1}(?=[A-Z]{1,})', line), '\n')
 
-print(re.findall('[a-z]+',line))
+az = 'abcdefghijklmnopqrstuvwxyz'
+AZ = az.upper()
+i = 0
+my_spisok = []
+try:
+    for j in line:
+        if AZ.count(line[i + 1]) and az.count(j):
+            my_spisok.append(j)
+        if AZ.count(line[i - 1]) and az.count(j):
+            my_spisok.append(j)
+        i += 1
+except:
+    print(my_spisok)
+    print('line[i + 1] Выходит за рамки длинной строки, поэтому try except \n')
 
 # Задание-2:
 # Вывести символы в верхнем регистре, которые окружают ровно два символа в нижнем регистре слева
 # и два символа в верхнем регистре справа. Решить задачу двумя способами: с помощью re и без.
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalp' \
-       'PLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzKTUfSYtBydXaVIpxWjNKgXANv' \
-       'IoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkp' \
-       'YOlqXEFIxPqqqgAWdJsOvqppOfyIVjXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSA' \
-       'fJMchgBWAsGnBnWetekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAb' \
-       'fCvzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXbJrwTRNyA' \
-       'xDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCCEUZShGofaFpuespaZWLFN' \
-       'IsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUf' \
-       'lwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZC' \
-       'nZjLeMiFlxnPkqfJFbCfKCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxy' \
-       'GPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXi' \
-       'UWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+         'PLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzKTUfSYtBydXaVIpxWjNKgXANv' \
+         'IoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkp' \
+         'YOlqXEFIxPqqqgAWdJsOvqppOfyIVjXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSA' \
+         'fJMchgBWAsGnBnWetekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAb' \
+         'fCvzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXbJrwTRNyA' \
+         'xDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCCEUZShGofaFpuespaZWLFN' \
+         'IsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUf' \
+         'lwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZC' \
+         'nZjLeMiFlxnPkqfJFbCfKCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxy' \
+         'GPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXi' \
+         'UWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+i = 1
+my_spisok2 = []
+try:
+    for j in line_2:
+        if AZ.count(line_2[i + 1]) and AZ.count(line_2[i + 2]) and AZ.count(j):
+            my_spisok2.append(j)
+        if az.count(line_2[i - 1]) and az.count(line_2[i - 2]) and AZ.count(j):
+            my_spisok2.append(j)
+        i += 1
+except:
+    print(my_spisok2)
+    print('line[i + 1] Выходит за рамки длинной строки, поэтому try except \n')
+
+print(re.findall('(?<=[a-z]{2})[A-Z]+(?=[A-Z]{2})', line_2), '\n')
+# Если честно, формулировка сложно укладывается в голове :(
 
 # Задача-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла) произвольными целыми
 # числами, в результате в файле должно быть 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр в вышезаполненном файле.
+
+import os
+import random
+
+path = os.path.join('files', 'text.txt')
+my_spisok3 = [str(random.randint(0, 9)) for _ in range(2500)]
+
+with open(path, 'w') as f:
+    for i in my_spisok3:
+        f.write(i)
+
+with open(path, 'r') as f:
+    i = 0
+    while i <= 9:
+        print(max(re.findall('[i]+', f.read()))) #почему не работает? Хочу вывести список с максимальным числом от повторяющихся от 0 до 9 цифр
+        i += 1
+
+    # print(max(re.findall('[0-9]+', f.read()))) #Можно ли эту задачу рещить с помощью рег.выражений?
