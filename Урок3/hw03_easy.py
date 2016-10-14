00c8a9906a9a6d977be0de3d3bfc16d03a8a1d42
Mikhail Garkusha <mgarkusha@mail.ru>
2016-10-14 16:55:38 +0300

    Update

--- Урок3/hw03_easy.py
+++ Урок3/hw03_easy.py
@@ -4,18 +4,19 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math
 
-#Кажется, моё решение через строки было в корне неправильным??? :) пока бросил, подскажите пожалуйста как решать
+# Кажется, моё решение через строки было в корне неправильным??? :) пока бросил, подскажите пожалуйста как решать
 def my_round(number, ndigits):
-	big_number = str(number * 10 ** (ndigits + 1)) # + 1 что-бы с точкой не встречаться
-	print(big_number)
-	print(big_number[ndigits + 1:ndigits + 2])
-	print(big_number[:ndigits + 1])
-	if int(big_number[ndigits + 1:ndigits + 2]) >= 5:
-		new_big_number = int(big_number[:ndigits + 1]) + 1
-	else:
-		new_big_number = int(big_number[:ndigits + 1])
-	out_number = new_big_number / 10 ** (ndigits)
-	print(out_number)
+    big_number = str(number * 10 ** (ndigits + 1))  # + 1 что-бы с точкой не встречаться
+    print(big_number)
+    print(big_number[ndigits + 1:ndigits + 2])
+    print(big_number[:ndigits + 1])
+    if int(big_number[ndigits + 1:ndigits + 2]) >= 5:
+        new_big_number = int(big_number[:ndigits + 1]) + 1
+    else:
+        new_big_number = int(big_number[:ndigits + 1])
+    out_number = new_big_number / 10 ** (ndigits)
+    print(out_number)
+
 
 my_round(22.2757, 3)
 
@@ -28,25 +29,26 @@ print('\n')
 
 
 def lucky_ticket(ticket_number):
-	'''
-	Фнукия сработает если билет счастливый
-	'''
-	lucky = 0
-	summ_one_hulf = 0
-	summ_two_hulf = 0
-	one_hulf = str(ticket_number)[:3]
-	two_hulf = str(ticket_number)[3:]
-	for number_one_hulf in one_hulf:
-		summ_one_hulf += int(number_one_hulf)
-	for number_two_hulf in two_hulf:
-		summ_two_hulf += int(number_two_hulf)
-	if summ_one_hulf == summ_two_hulf:
-		lucky += 1
-	return lucky
-		
+    '''
+    Фнукия сработает если билет счастливый
+    '''
+    lucky = 0
+    summ_one_hulf = 0
+    summ_two_hulf = 0
+    one_hulf = str(ticket_number)[:3]
+    two_hulf = str(ticket_number)[3:]
+    for number_one_hulf in one_hulf:
+        summ_one_hulf += int(number_one_hulf)
+    for number_two_hulf in two_hulf:
+        summ_two_hulf += int(number_two_hulf)
+    if summ_one_hulf == summ_two_hulf:
+        lucky += 1
+    return lucky
+
+
 ticket = int(input('Введите 6-ти значный номер билета: '))
 
 if (lucky_ticket(ticket)):
-	print('Ваш билет счастливый!!!')
+    print('Ваш билет счастливый!!!')
 else:
-	print('Не счастливый')
+    print('Не счастливый')
--- Урок3/hw03_normal.py
+++ Урок3/hw03_normal.py
@@ -3,14 +3,16 @@
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-	my_list = [1, 1]
-	i = 2
-	while i < m:
-		my_list.append(my_list[i - 1] + my_list[i - 2])
-		i += 1
-	return print(my_list)
+    my_list = [1, 1]
+    i = 2
+    while i < m:
+        my_list.append(my_list[i - 1] + my_list[i - 2])
+        i += 1
+    return print(my_list)
+
 
 fibonacci(1, 10)
+
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -18,13 +20,14 @@ fibonacci(1, 10)
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
 def sort_to_max(origin_list):
-	for i in origin_list:
-		j = 0
-		while j < (len(origin_list) - 1):
-			if origin_list[j] > origin_list [j + 1]:
-				origin_list[j], origin_list[j + 1] =  origin_list[j + 1], origin_list[j]
-			j += 1
-	return print(origin_list)
+    for i in origin_list:
+        j = 0
+        while j < (len(origin_list) - 1):
+            if origin_list[j] > origin_list[j + 1]:
+                origin_list[j], origin_list[j + 1] = origin_list[j + 1], origin_list[j]
+            j += 1
+    return print(origin_list)
+
 
 sort_to_max([10, 2, 1, -1.6, 40, 5])
 
@@ -33,18 +36,18 @@ sort_to_max([10, 2, 1, -1.6, 40, 5])
 # Напишите собственную реализацию функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 def my_filter(numbers, number, condition):
-	new_numbers = []
-	for i in numbers:
-		if condition == '>':
-			if i > number:
-				new_numbers.append(i)
-		elif condition == '<':
-			if i < number:
-				new_numbers.append(i)
-		elif condition == '=':
-			if i == number:
-				new_numbers.append(i)
-	return print(new_numbers)
+    new_numbers = []
+    for i in numbers:
+        if condition == '>':
+            if i > number:
+                new_numbers.append(i)
+        elif condition == '<':
+            if i < number:
+                new_numbers.append(i)
+        elif condition == '=':
+            if i == number:
+                new_numbers.append(i)
+    return print(new_numbers)
 
 
 numbers = [1, 2, 4, 5, 5, 10, 20]
@@ -53,4 +56,3 @@ my_filter(numbers, 5, '=')
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
-
